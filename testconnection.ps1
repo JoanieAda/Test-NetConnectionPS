d5c0bd174e3349ea12f57cd6288e51f9febae3ef
Joanie Sylvain <joaniesylvain8@gmail.com>
2021-03-18 06:50:33 -0400

    Added reporting function and redesigned the loop for testing

--- testconnection.ps1
+++ testconnection.ps1
@@ -1,13 +1,27 @@
-<#
-CSV file format
+<# 
+testconnnection.ps1 -path <csv file> [-ntp] [-report]
 
+This script was built to perform some validation tests based on an csv host list.
+-path is a mandatory value
+-ntp will check the NTP status of the host (may or may not be relevant based on the host config)
+-report will save the output file dated of today (output is appended)
+
+CSV file format
 IP;Port;Ping
 serverip;80;yes
+
+Set Port to '0' to prevent TCP test
+Set Ping to 'no' to prevent Ping test
+
+IP can also be an fqdn as long as the host can perform proper name resolution
 
 #>
 
 param (
-    [Parameter(Mandatory=$false)] [switch] $ntp,
+    [Parameter(Mandatory=$false)] 
+    [switch] $ntp,
+    [Parameter(Mandatory=$false)] 
+    [switch] $report,
 
     [Parameter(Mandatory=$true)]
     [ValidateScript({ 
@@ -17,13 +31,19 @@ param (
 );
 
 
-<# Static variables for testing
-$IP = '192.168.1.10'
-$Port = '80'
-$Ping = 'yes'
-#>
+#Generates report path and inserts a leading date line
+if ($report){
+
+    $originalpath = Get-Item $path
+    $exportpath = Join-Path -Path $originalpath.DirectoryName -ChildPath $("Report_"+ $originalpath.BaseName + "_" + (Get-Date).tostring("yyyy-MM-dd") + ".log") 
+    write-output ('------------' + (Get-Date) + '------------') | Out-File -Append $exportpath
+
+}
+else{
+}
 
 
+#Check the reachability/TCP connection for the hosts in the CSV
 $serverlist=Import-Csv -Path $path -Delimiter ";" -ErrorAction Stop
 
 $serverlist| ForEach-Object {
@@ -31,14 +51,22 @@ $serverlist| ForEach-Object {
         if ($_.Ping -eq 'yes'){         
             if (Test-NetConnection $_.IP -InformationLevel Quiet -ErrorAction Stop -WarningAction SilentlyContinue){
 
-                write-host -ForegroundColor Green $_.IP"ping successful"
-
-        }
-            else{
-
-                write-host -ForegroundColor Red $_.IP"ping failed"
+                $string = ($_.IP + ' ping successful')
+                $stringcolor = 'Green'
 
             }
+            else{
+
+                $string = ($_.IP + ' ping failed')
+                $stringcolor = 'Red'
+
+            }
+
+            write-host -ForegroundColor $stringcolor $string
+            if ($report){
+                write-output $string | Out-File -Append $exportpath
+            }
+
         }
         else{            
         }
@@ -46,14 +74,22 @@ $serverlist| ForEach-Object {
         if ($_.Port -gt '0'){
             if (Test-NetConnection $_.IP -Port $_.Port -InformationLevel Quiet -ErrorAction Stop -WarningAction SilentlyContinue){
 
-                write-host -ForegroundColor Green $_.IP"TCP connection to port"$_.Port"established"
+                $string = ($_.IP + ' TCP connection to port ' + $_.Port + ' established')
+                $stringcolor = 'Green'
 
             }
             else{
 
-                write-host -ForegroundColor Red $_.IP"TCP connection to port"$_.Port"failed"
+                $string = ($_.IP + ' TCP connection to port ' + $_.Port + ' failed')
+                $stringcolor = 'Red'
 
             }
+
+            write-host -ForegroundColor $stringcolor $string
+            if ($report){
+                write-output $string | Out-File -Append $exportpath
+            }
+
         }
         else{
         }
@@ -64,6 +100,7 @@ $serverlist| ForEach-Object {
 }
 
 
+#Check NTP status of the host 
 if ($ntp){
 
     $ntpstate = w32tm /query /peers | select-string "state:"
@@ -71,29 +108,28 @@ if ($ntp){
 
     if ($ntpstate -like '*Active'){
 
-        write-host -ForegroundColor Green "NTP Server $ntppeer Active"
+        $string = ('NTP Server ' + $ntppeer + ' active')
+        $stringcolor = 'Green'
 
     }
     else{
 
-        write-host -ForegroundColor Red "NTP Server $ntppeer Failed"
+        $string = ('NTP Server ' + $ntppeer + ' not conencted')
+        $stringcolor = 'Red'
 
     }
+
+    write-host -ForegroundColor $stringcolor $string
+    if ($report){
+        write-output $string | Out-File -Append $exportpath
+    }
+
 }
 else{
 }
 
 
-<#
-
-
-$outputObject=Get-Item $Path
-
-#Build file name for the report
-$exportPath= Join-Path -Path $outputObject.DirectoryName -ChildPath $("Report_"+ $outputObject.BaseName + $outputObject.Extension)
-
-$checkList | Export-Csv -Path $exportPath -NoTypeInformation -Delimiter ";" -ErrorAction Stop
-
-#>
-
-
+#Add new line at the end of the report
+if ($report){
+    write-output `n | Out-File -Append $exportpath
+}
